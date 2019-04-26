ebb7df10bce946e761454ce8462ce14f17ff7037
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2019-04-25 22:41:58 -0700

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+	//This function gets run once at the start of the sketch
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+	//This function gets run over and over again
+  background(220); //give the canvas a grey background
+
+
+}
