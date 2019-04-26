934d0facefa4e8da2de90c2a55f94a8ac2122bfe
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2019-04-25 23:02:21 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,10 @@
 function setup() {
-	//This function gets run once at the start of the sketch
-  createCanvas(400, 400);
-
+	//This function get run once at the start of the program
+	createCanvas(400, 400);
 }
 
 function draw() {
-	//This function gets run over and over again
-  background(220); //give the canvas a grey background
-
+	//This loop gets run over and over again
+	background(220);
 
 }
